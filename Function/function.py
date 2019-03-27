0b9b483e921fe376e5d07c9fc0f7c4459eee92c5
kashif <naqvishah582@gmail.com>
2019-03-27 20:37:33 +0500

    add more function examples in it

--- Function/function.py
+++ Function/function.py
@@ -14,3 +14,55 @@ print(name)
 # Hello world
 # None
 # <function name at 0x7f665c216950>
+
+
+# ---------------------------NEW EXAMPLE---------------------------------
+
+def squre(x):
+  return x * x
+
+print(squre(3))
+
+# Output:9
+#---------------------------NEW EXAMPLE-----------------------------------
+
+def cube(x):
+  return x*x*x
+
+print(cube(4))
+
+# Output:64
+
+# --------------------------NEW EXAMPLE------------------------------------
+def name(arg1,arg2):
+  print(arg1,"  ",arg2)
+
+print(name("kashif","Shahzad"))
+
+# output: Kashif  Shahzad
+
+#--------------------------NEW EXAMPLE-------------------------------------
+def power(num,x):
+  result = 1 
+  for i in range(x):
+    result = result * num
+  return result
+
+print (power(2,2))
+
+# output:4
+
+# ------------------------NEW EXAMPLE---------------------------------------
+
+def multipleData(*arg):
+  result = 0
+  for x in arg:
+    result = result + x
+  return result
+print(multipleData(2,2,7))
+
+# output: 11
+
+# -----------------------END EXAMPLES----------------------------------------
+
+print("Kashif Naqvi By You learn a lot from this chapter i think :) ")
