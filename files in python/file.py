a455891820164fef54bb720072a0d010812da9c7
kashif <naqvishah582@gmail.com>
2019-04-01 18:35:06 +0500

    working wi files

---
+++ files in python/file.py
@@ -0,0 +1,20 @@
+
+def main():
+  # write a file
+  # f = open("textfile.txt","w+")
+  
+  # add data in existing file
+
+  # f = open("textfile.txt","a")
+  # for i in range(10):
+  #   f.write("this is code for learning "+str(i)+ "\n\r")
+  # f.close()
+
+
+  f = open("code.txt","w+")
+
+  f.write("code is art")
+  f.close()
+
+if __name__ == "__main__":
+    main()
